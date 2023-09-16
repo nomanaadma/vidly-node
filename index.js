1c61cae87a64d1c6248073a3cfcbfa7315b43789
Noman Shoukat <nomanaadma@gmail.com>
2023-09-16 11:18:24 +0500

    single course req

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const express = require('express');
 
 const app = express();
 
+const courses = [
+    { id: 1, name: 'course 1'},
+    { id: 2, name: 'course 2'},
+    { id: 3, name: 'course 3'},
+]
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send('api courses');
+    res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id) );
+    
+    if(!course) res.status(404).send('The course with given id was not found.');
+
+    res.send(course);
 });
 
 
