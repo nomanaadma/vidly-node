0dbece5f6fa1bddba7a13982e28cc6e40bce02d4
Noman Shoukat <nomanaadma@gmail.com>
2023-09-18 18:21:58 +0500

    genre model

---
+++ models/genre.js
@@ -0,0 +1,27 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50,
+        trim: true,
+    },
+}));
+
+function validateGenre(genre) {
+
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return schema.validate(genre);
+
+}
+
+
+
+exports.Genre = Genre;
+exports.validate = validateGenre;
--- routes/genres.js
+++ routes/genres.js
@@ -1,19 +1,6 @@
 const express = require('express');
-const Joi = require('joi');
-const mongoose = require('mongoose');
 const router = express.Router();
-
-
-const Genre = mongoose.model('Genre', new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 50,
-        trim: true,
-    },
-}));
-
+const { Genre, validate } = require('../models/customer');
 
 router.get('/', async (req, res) => {
     const genres = await Genre.find().sort('name');
@@ -22,7 +9,7 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
 
-    const { error } = validateGenre(req.body);
+    const { error } = validate(req.body);
 
     if(error) return res.status(400).send(error.details[0].message);
 
@@ -45,7 +32,7 @@ router.get('/:id', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
 
-    const { error } = validateGenre(req.body);
+    const { error } = validate(req.body);
 
     if(error) return res.status(400).send(error.details[0].message);
     
@@ -69,17 +56,6 @@ router.delete('/:id', async (req, res) => {
     res.send(genre);
 
 });
-
-
-function validateGenre(genre) {
-
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-
-    return schema.validate(genre);
-
-}
 
 
 module.exports = router;
