659226b330280679491884d03a75b32779c12a28
Noman Shoukat <nomanaadma@gmail.com>
2023-09-19 16:32:57 +0500

    authentication

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const home = require('./routes/home');
 const app = express();
 
@@ -32,8 +33,8 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 app.use('/', home);
-
 
 
 const port = process.env.PORT || 3100;
