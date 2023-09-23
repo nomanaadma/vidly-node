e10c24442108064c5aceba4671610add5afa9f08
Noman Shoukat <nomanaadma@gmail.com>
2023-09-23 14:43:31 +0500

    simple error handling

--- routes/genres.js
+++ routes/genres.js
@@ -5,8 +5,14 @@ const router = express.Router();
 const { Genre, validate } = require('../models/genre');
 
 router.get('/', async (req, res) => {
-    const genres = await Genre.find().sort('name');
-    res.send( genres );
+    
+    try {
+        const genres = await Genre.find().sort('name');
+        res.send( genres );
+    } catch(ex) {
+        res.status(500).send('Something Failed.');
+    }
+
 });
 
 router.post('/', auth, async (req, res) => {
